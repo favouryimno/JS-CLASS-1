a04aa9f39895b594af98d570690bd3faf45196da
favour dedan <favouryimno@gmail.com>
2021-05-18 10:42:19 +0100

    initial commit

---
+++ classic.js
@@ -0,0 +1,11 @@
+var a; // declaration
+a = 5; // assignment
+
+console.log(a); // printing to console
+
+var b = "this is javascript"; // Declaration and assignment
+
+console.log(b);
+
+let c = 100;
+console.log(c)
