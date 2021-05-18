3600a0898300e438a92fead473fe50350fffc429
favour dedan <favouryimno@gmail.com>
2021-05-18 15:43:18 +0100

    Third commit

---
+++ Assignment.js
@@ -0,0 +1,30 @@
+var a;
+var b;
+var c;
+
+//string
+a = 'Favour Dedan'
+console.log(a)
+console.log(typeof(a))
+
+//integers
+a = 70
+console.log(a)
+console.log(typeof(a))
+
+//floats
+a = 168.983
+console.log(a)
+console.log(typeof(a))
+
+//boolean
+a = 98
+b = 45
+c = a > b
+console.log(c)
+console.log(typeof(c))
+
+
+    
+ 
+
