d1161e93649d0e507fa2b04c9bf7171034c742a0
favour dedan <favouryimno@gmail.com>
2021-05-19 16:21:18 +0100

    third commit

---
+++ Assignment2.js
@@ -0,0 +1,15 @@
+var a = 45;
+var b = 12;
+if (a > b) {
+    console.log("a is greater than b");
+} else if (a < b) {
+    console.log("a is less than b");
+}
+
+var age1 = 26;
+var age2 = 14;
+if (age2 >= 18 ) {
+    console.log("congratulations! you are admitted into university");
+}else if (age2 < 18) {
+    console.log("Admission not granted!");
+}
---
+++ jsclass2.js
@@ -0,0 +1,35 @@
+console.log(5 <= 5);
+
+//AND
+
+//x and y | result
+//true and true | true
+//true and false | false
+//false and true | false
+//false and false | false
+
+var age = 25;
+var gender = 'm'
+
+if (age > 18 && gender == 'm') {
+    console.log('you can join the game cos you are ' +age +'years old')
+    } else {
+        console.log('you can go home and play at home')
+    }
+    if (age >= 18 && gender == 'm') {
+        console.log('you can play ps5 cos you are ' +age +'years old')
+        } else if ( age < 18 && age > 10) {
+            console.log('you can play ps4 cos you are ' +age +'years old')
+    }else {
+            console.log('you can go home and play at home')
+        }
+
+        //OR
+
+        // x or y | result
+        //true or true | true
+        //true or false | true
+        //false or true | true
+        //fakse or false | false
+    
+    
